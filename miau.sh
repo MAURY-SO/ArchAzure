60582208a2fc60a555051dd2c5a51872e9d2990a
Maury SO <azuralynux@gmail.com>
2024-08-19 09:15:34 -0600

    3

--- miau.sh
+++ miau.sh
@@ -8,13 +8,24 @@ Installer Arch User
     clear               
     cat banner.txt        
 
+#Disco
+    discosdisponibles=$(echo "print devices" | parted | grep /dev/ | awk '{if (NR!=1) {print}}' | sed '/sr/d')
+    clear
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+    echo ""
+    echo "Rutas de Disco disponible: "
+    echo ""
+    echo $discosdisponibles
+    echo ""
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+
 #Usuario                
     # echo ""
     # read -p "Introduce tu disco a instalar Arch: " disco
     echo ""
-    read -p "Introduce Nombre usuario Nuevo: " user
+    read -p "Username: " user
     echo ""
-    read -p "Introduce la clave de $user / root: " passwd
+    read -p "Password        $user / root: " passwd
     echo ""
 
 #Idioma del sistema
