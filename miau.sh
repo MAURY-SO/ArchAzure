150019a2372424cdccd298e2122dbea52432ff13
Maury SO <azuralynux@gmail.com>
2024-08-19 14:57:13 -0600

    USERS

--- miau.sh
+++ miau.sh
@@ -4,26 +4,49 @@
 Installer Arch User
 
 '
+
+#STYLE
+
+# Definir códigos de color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+BOLD='\033[1m'
+
 #--->Banner
     clear               
     cat banner.txt        
-
-#Disco      
-    discosdisponibles=$(echo "print devices" | parted | grep /dev/ | awk '{if (NR!=1) {print}}' | sed '/sr/d')
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
-    echo ""
-    echo "Rutas de Disco disponible: "
-    echo ""
-    echo $discosdisponibles
-    echo ""
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
 
-#Usuario                
+#Usuario
+    echo -e "${BOLD}USER CONFIGURATION${RESET}"      
+    echo ""          
+    read -p "username-> " user_aux
     echo ""
-    read -p "Disk: " disk       
-    read -p "Username: " user
-    read -p "Password $user-root: " passwd
+    user=${user_aux,,}
+    echo -e "Your username --> ${BOLD}$user${RESET}"
     echo ""
+    echo "Enter the root - $user password (your password is hide)"
+    loop=true
+    while $loop;
+    do
+        read -sp "password-> " passwd
+        echo ""
+        read -sp "retype your password-> " passwd_aux
+        echo ""
+        
+        if [ "$passwd" = "$passwd_aux" ]; then
+            echo ""
+            echo -e "${GREEN}${BOLD}Password pased!!!${RESET}"
+            loop=false
+        else
+            echo ""
+            echo -e "${RED}${BOLD}Password incorrect!!!${RESET}"
+            echo ""
+        fi
+
+    done       
+    echo ""                    
 
 #Idioma del sistema
     idioma=$(curl https://ipapi.co/languages)".UTF8"
@@ -38,10 +61,22 @@ Installer Arch User
         echo "El sistema es BIOS"
     fi
 
+#Disco      
+    # discosdisponibles=$(echo "print devices" | parted | grep /dev/ | awk '{if (NR!=1) {print}}' | sed '/sr/d')
+    # printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+    # echo ""
+    # echo "Rutas de Disco disponible: "
+    # echo ""
+    # echo $discosdisponibles
+    # echo ""
+    # printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+    # echo ""
+    # read -p "Disk: " disk 
+
 #Particionado
 
     #UEFI
-    
+
 
 #Instalar paquetes post-instalacion
     #sudo pacman -S --needed -< packages.txt
